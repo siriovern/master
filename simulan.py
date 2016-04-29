09a9c87afe3f7a5f702690e8d505b3e98549e898
siriovern <siri.overn@gmail.com>
2016-04-29 10:05:07 +0200

    fikser average
    kode for plot er korrekt. jobber med gjennomsnittsverdien per 1/T

--- simulan.py
+++ simulan.py
@@ -1,14 +1,19 @@
 __author__ = 'Siri'
 
 import math, random
+import matplotlib.pyplot as plt
+import numpy as np
 
 # boltzmann at start
 kT = 10
 #number of cycles
-n = 50
+n = 90
+#number of trials per cycle
+m = 50
 #boltzmann at the end
 kTend = 1
 
+avgList = list()
 start = 2000
 current = start
 
@@ -39,15 +44,38 @@ def numreactions(reactions):
 
 #best results so far
 allReactions = [0] * n
+allTemperatures = [0] * n
+
 
 for i in range(n):
-    new, reactionvalue = numreactions(current)
-    deltaE = abs(start-reactionvalue)
-    prob = math.exp(-deltaE/kT)
-    if (random.random()<prob):
-        current = new
-    allReactions[i] = current
-    kT = kT-0.1
+    while kT >= kTend:
+        for j in range(m):
+
+            new, rxValue = numreactions(current)
+            deltaE = abs(start-rxValue)
+            prob = math.exp(-deltaE/kT)
+            if (random.random()<prob):
+                current = new
+                avgList.append(current)
+            else:
+                continue
+
+        avgNumber = np.mean(avgList)
+        avgList = []
+        allReactions[i] = avgNumber
+        xAxis = (1/kT)
+        allTemperatures[i] = xAxis
+        kT = kT-0.1
+
+stdev = [0]*n
+#stdev
+#for i in range(n):
+ #   stdev[i] =
 
 print allReactions
+print allTemperatures
+
+plt.scatter(allTemperatures,allReactions)
+plt.axis([0,1, 1990,2010])
+plt.show()
 
