0031bbaf2695dd03d994ff9b15238bd12a0112bd
siriovern <siri.overn@gmail.com>
2016-11-09 10:50:01 +0100

    printer ut antall reaksjoner

--- cobra_code.py
+++ cobra_code.py
@@ -1,28 +1,33 @@
 __author__ = 'Siri'
 
 import random
-import cobra
+import cobra.test
 import pandas
 
+model = cobra.test.create_test_model(cobra.test.salmonella_pickle)
 
-def numberOfReactions():
+def numberofreactions():
 
-    # finn antall reaksjoner fra modell M
+    numberReactions = len(model.reactions) # antall reaksjoner i model
 
-    addOrRemove = random.random
+    addOrRemove = random.random()
 
     if addOrRemove >= 0.5:
         # add reaction
+        print numberReactions
     else:
         # remove reaction
         # kan modellen fremdeles produsere biomass?
+        print numberReactions
+
 
 
     return (numberReactions)
 
-def numberOfBlockedReactions(number):
+def numberofblockedreactions():
 
     # finn antall blokkerte raksjoner fra navaerende modell M
     # enklest: sjekke flux for ALLE reaksjoner (veldig tregt!)
+    blockedReactions = 1
 
     return(blockedReactions)
