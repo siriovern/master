d87229290098957801e991f2b1ccb10de08b3957
siriovern <siri.overn@gmail.com>
2016-11-09 10:51:39 +0100

    første utkast med print
    printer ut antall reaksjoner. antall blokkerte reaksjoner er bare satt
    til 1

--- simulated_annealing_code.py
+++ simulated_annealing_code.py
@@ -3,8 +3,8 @@ __author__ = 'Siri'
 
 import math
 import random
-from cobra_code import numberOfReactions
-from cobra_code import numberOfBlockedReactions
+from cobra_code import numberofreactions
+from cobra_code import numberofblockedreactions
 
 temp1 = 1 # for antall reaksjoiner
 temp2 = 1 # for blokkerte reaksjoner. Holdes konstant
@@ -13,33 +13,39 @@ optimalReactions = 1 # onsket antall reaksjoner
 optimalBlocked = 1 # maks antall blokkerte reaksjoner
 
 
-n=50 # Antall sykler per temp. Like mange som antall reaksjoner i opprinnelig modell?
+n=1 # Antall sykler per temp. Like mange som antall reaksjoner i opprinnelig modell?
 
 while temp1 != 0:
 
     for i in range(n):
-        nReactions = numberOfReactions() #kaller funksjon fra cobra_code
-        nBlocked = numberOfBlockedReactions()
+        nReactions = numberofreactions() #kaller funksjon fra cobra_code
+        nBlocked = numberofblockedreactions()
         if nReactions != optimalReactions:
             deltaN = math.fabs(optimalReactions-nReactions)
             p1 = math.exp(-deltaN/temp1) # sannsynlighet som funksjon av t1
-            randomNumber1 = random.random
+            randomNumber1 = random.random()
             if randomNumber1 > p1:
+                print nReactions
                 # forkast sletting av reaksjon
             else:
                 # behold endringer
+                print nReactions
         else:
+            print nReactions
             # keep
 
         if nBlocked > optimalBlocked:
             deltaB = optimalBlocked-nBlocked
             p2 = math.exp(deltaB/temp2)
-            randomNumber2 = random.random
+            randomNumber2 = random.random()
             if randomNumber2 > p2:
+                print nBlocked
                 # forkast sletting av reaksjon
             else:
+                print nBlocked
                 # behold endringer
         else:
+            print nBlocked
             # keep
     temp1 = temp1 -0.1
 
