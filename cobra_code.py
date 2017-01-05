9c3693869161dc544bcc09f1876ed94da33f7451
siriovern <siri.overn@gmail.com>
2017-01-05 12:26:06 +0100

    hurra
    fjerner reaksjon fra univers og legger til i modell

--- cobra_code.py
+++ cobra_code.py
@@ -2,44 +2,65 @@ __author__ = 'Siri'
 
 import random
 import cobra.test
+import copy
 #from cobra.io.mat import model_to_pymatbridge
 
 import pandas
 #model_to_pymatbridge(m, variable_name="model")
 
-model = cobra.test.create_test_model(cobra.test.salmonella_pickle)
-# laste inn naavaerende modell
-# laste inn univers
+from cobra import Reaction
+
+salmonella = cobra.test.create_test_model(cobra.test.salmonella_pickle)
+model = copy.copy(salmonella)
+universe = cobra.io.read_sbml_model('C:\Users\Siri\SkyDrive\NTNU\_master\universe_mnx.xml')
 
 def numberofreactions():
 
 
-    numberReactions = len(model.reactions) # antall reaksjoner i model
-
     biomass = model.objective # = 1 hvis modellen produserer biomass
-    opt = model.optimize()
+    #opt = model.optimize()
 
-    print 'her kommer opt.f:'
-    print opt.f
-    print model.solution.f
-
+    #print 'her kommer opt.f:'
+    #print opt.f #samme som     print model.solution.f
+    print 'biomass: 1 if biomass:'
     print biomass
 
-   # if biomass == 'Reaction biomass_iRR1083_metals at 0x75f9790>: 1.0':
-    #    print 'hurra'
-
     addOrRemove = random.random()
+    #print addOrRemove
+    nModel = len(model.reactions) # antall reaksjoner i model
+    nUniverse = len(universe.reactions)
 
-    if addOrRemove >= 0.5:
-        # add reaction
-        print numberReactions
+    if addOrRemove >= 0.1: # add reaction
+
+        reactionNr = random.randint(1, nUniverse)
+        react = universe.reactions[reactionNr] #velger tilfeldig reaksjon i universet
+        reaction = Reaction(react)
+        print 'reaksjoner i modell: %d' % (nModel)
+        print 'reaksjoner i univers: %d' % (nUniverse)
+       # reaction.remove_from_model(universe)
+
+        print reaction
+
+        model.add_reaction(reaction) #legger til reaksjon i modell
+
+        universe.remove_reactions(reaction) #fjerner reaksjon fra univers
+        universe.reactions.remove(react)
+
+
+        mModel = len(model.reactions) #sjeker ny lengde
+        print 'reaksjoner i modell er naa: %d' % (mModel)
+        mUniverse =  len(universe.reactions) #ny lengde univers
+        print 'reaksjoner i unvers er naa: %d' % (mUniverse)
+
+
+        return nModel
     else:
         # remove reaction
         # kan modellen fremdeles produsere biomass?
-        print numberReactions
+        print nModel
 
 
-    return (numberReactions)
+    return (nModel)
 
 def numberofblockedreactions():
 
